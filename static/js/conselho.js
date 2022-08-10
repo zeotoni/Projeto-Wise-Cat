9d1dae0a1c316d4063d0df7bfa307a87590e64c5
ntbraga <pedro.braga@grupoboticario.com.br>
2022-08-10 13:24:01 -0300

    fix: correção imagem e conselho

--- static/js/conselho.js
+++ static/js/conselho.js
@@ -4,33 +4,36 @@ let exibirImagem = document.getElementById('container__conselho')
 const containerImg = document.querySelector('#container-imagem')
 
 function exibeConselho() {
-    
+
     fetch("https://api.adviceslip.com/advice")
-    .then(response => {
-        return response.text();
-    })
-    .then(function(objeto) {
+        .then(response => {
+            return response.json();
+        })
+        .then(function (objeto) {
+            // Formato do objeto retornado
+            // { "slip": { "id": 9, "advice": "True happiness always resides in the quest." } }
+            // Quando o valor da variável não muda, é interessante usar const
+            const found = objeto.slip.advice;
+            exibirConselho.innerHTML = found;
+        })
+        .catch(error => console.log(error))
 
-        let found = objeto.match(/[^{}:\d][A-Z].+"/);
-        exibirConselho.innerHTML = found;
-    })
-    .catch(error => console.log(error))
 
-    
     var myImage = document.querySelector('[data-img]');
     let url = `https://api.thecatapi.com/v1/images/search`;
     fetch(url)
-    .then(function(response) {
-        return response.blob();
-    })
-    .then(function(response) {
-        // let newUrl = response.url
-        // console.log(newUrl)
-        let novaImg = document.createElement('img');
-        novaImg.innerHTML = `<img src="${url}" alt="Imagem de um gatinho" class="imagem__gato"></img>`
-        containerImg.appendChild(novaImg);
-    }) 
-    .catch(error => console.log(error))
+        .then(function (response) {
+            // Aqui, a response dessa api, não é uma imagem, é um json
+            // [{"id": "MjAyNTY4Ng","url": "https://cdn2.thecatapi.com/images/MjAyNTY4Ng.jpg","width": 720,"height": 540}]
+            return response.json();
+        })
+        .then(function (response) {
+            //Array.shift remove o primeiro item de um array e retorna;
+            const primeiroItemResposta = response.shift();
+            const urlImagemGatinho = primeiroItemResposta.url;
+            myImage.src = urlImagemGatinho;
+        })
+        .catch(error => console.log(error))
 
 }
 
